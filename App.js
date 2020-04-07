38da640fa583f0196318cf1469f9608499e28129
RonnyFalconeri <34677325+RonnyFalconeri@users.noreply.github.com>
2020-04-07 14:46:20 +0200

    react-navigation installed

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to AdditionalScreen1"
+        onPress={() => navigation.navigate('AddScreen1')}
+      />
     </View>
   );
 }
+
+function AdditionalScreen1({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Additional Screen 1</Text>
+      <Button
+        title="Go to AdditionalScreen2"
+        onPress={() => navigation.navigate('AddScreen2')}
+      />
+    </View>
+  );
+}
+
+function AdditionalScreen2() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Additional Screen 2</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddScreen1" component={AdditionalScreen1} />
+        <Stack.Screen name="AddScreen2" component={AdditionalScreen2} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {

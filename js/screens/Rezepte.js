0dc066e6f9f85ef448aae8c0a63a3d33332a4a6a
RonnyFalconeri <34677325+RonnyFalconeri@users.noreply.github.com>
2020-04-16 17:01:23 +0200

    minor changes

--- js/screens/Rezepte.js
+++ js/screens/Rezepte.js
@@ -4,9 +4,9 @@ import { ScrollView, StyleSheet, DevSettings } from 'react-native';
 import Rezepte_ListItem from '../components/Rezepte_ListItem';
 
 const recipes = [
-    {title: "Spaghetti Bolognese", duration: "14Min", nationality:"ITA", image: "fd", onPress:"fjj"},
-    {title: "gjj", duration: "gjf", nationality:"gfk", image: "fkh", onPress:"hk"},
-    {title: "zjh", duration: "jjjj", nationality:"jhj", image: "ghk", onPress:"jjj"}
+    { title: "Spaghetti Bolognese", duration: "14Min", nationality: "ITA", image: "fd", onPress: "fjj" },
+    { title: "gjj", duration: "gjf", nationality: "gfk", image: "fkh", onPress: "hk" },
+    { title: "zjh", duration: "jjjj", nationality: "jhj", image: "ghk", onPress: "jjj" }
 ];
 
 export default class Rezepte extends React.Component {
@@ -21,15 +21,15 @@ export default class Rezepte extends React.Component {
         const ds = recipes;
         return (
             <ScrollView contentContainerStyle={styles.container}>
-                {ds.forEach(recipe => 
-                    <Rezepte_ListItem
-                    title = {recipe.title}
-                    duration = {recipe.title}
-                    nationality = {recipe.title}
-                    image = {recipe.title}
-                    onPress = {recipe.title}
-                     />
-                )};
+                {ds.forEach(recipe =>
+                    < Rezepte_ListItem
+                        title={recipe.title}
+                        duration={recipe.title}
+                        nationality={recipe.title}
+                        image={require('../../assets/icon.png')}
+                        onPress={() => this.props.navigation.navigate('Rezept')}
+                    />
+                )}
             </ScrollView>
         );
     }

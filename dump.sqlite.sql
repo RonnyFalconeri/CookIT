13d5935431085694eda9220e428e15dfa80c98a2
Mert69 <34682486+Mert69@users.noreply.github.com>
2020-04-20 10:45:01 +0200

    Create dump.sqlite.sql

---
+++ dump.sqlite.sql
@@ -0,0 +1,9 @@
+-- TABLE
+CREATE TABLE recipes(id integer primary key, name text, Duration TEXT, Ingrediens TEXT, Picture TEXT, Nationality TEXT, Preparation TEXT);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 

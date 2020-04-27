e43f02820b75464251507388aec33f8d885862fa
RonnyFalconeri <34677325+RonnyFalconeri@users.noreply.github.com>
2020-04-27 16:42:55 +0200

    new backgrounds on homepage

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -18,42 +18,42 @@ export default class HomeScreen extends React.Component {
                     title="Rezepte"
                     onPress={() => this.props.navigation.navigate('Rezepte')}
                     color="#6d9eeb"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/rezepteBackground.png')}
                 />
 
                 <HomeScreen_ListItem
                     title="Neues Rezept"
                     onPress={() => this.props.navigation.navigate('Neues_Rezept', { edit: false })}
                     color="#93c47d"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/neuesRezeptBackground.png')}
                 />
 
                 <HomeScreen_ListItem
                     title="Favoriten"
                     onPress={() => this.props.navigation.navigate('Favoriten')}
                     color="#8e7cc3"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/favoritenBackground.png')}
                 />
 
                 <HomeScreen_ListItem
                     title="Online-Suche"
                     onPress={() => this.props.navigation.navigate('Online_Suche')}
                     color="#76a5af"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/onlineSucheBackground.png')}
                 />
 
                 <HomeScreen_ListItem
                     title="Zufällig"
                     onPress={() => this.props.navigation.navigate('Zufällig')}
                     color="#f6b26b"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/zufälligBackground.png')}
                 />
 
                 <HomeScreen_ListItem
                     title="Einstellungen"
                     onPress={() => this.props.navigation.navigate('Einstellungen')}
                     color="#dd7e6b"
-                    image={require('../../assets/images/backgroundimage.png')}
+                    image={require('../../assets/images/einstellungenBackground.png')}
                 />
 
             </ScrollView>
